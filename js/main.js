efe978243ef32e1e63b6f04c6ca8bfb16c44a329
tareq0349 <tareqbhuiyan2021@gmail.com>
2019-07-27 10:22:58 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,109 @@
+// js datatypes
+// string, Number, boole
+//document.write('hello world');
+//console.log('Hello bangladesh');
+//window.alert( 'this is bad boy');
+//alert('hello feni');
+
+// variable
+//var x = 'true';
+//document.write(typeof(x));
+//document.write("<br>")
+//var y = 20;
+//document.write(typeof(y));
+
+//Operator
+// arithmatical operator( +,-,*,/,%,)
+// var x = 30;
+ //var y = 70;
+//document.write(x+y);
+//document.write("<br>");
+//document.write(x-y);
+//document.write("<br>");
+//document.write(x*y);
+//document.write("<br>");
+//document.write(x/y);
+//document.write("<br>");
+//document.write(x%y);
+//document.write("<br>");
+
+//var x =35;
+//document.write(x++);35
+//document.write("<br>");
+//document.write(x--);36
+//document.write("<br>");
+//document.write(x++);35
+//document.write("<br>");
+//document.write(x--);36
+//document.write("<br>");
+
+//assignment operatior
+/*
+var x=50
+var y=50
+
+document.write(x+=y);
+document.write("<br>");
+document.write(x-=y);
+document.write("<br>");
+document.write(x*=y);
+document.write("<br>");
+document.write(x/=y);1
+document.write("<br>");
+document.write(x%=y);
+document.write("<br>");
+
+//comparision operator
+var x =20;
+var y =28;
+document.write(x>y); false
+document.write("<br>");
+document.write(x<y);true
+document.write("<br>");
+document.write(x>=y); false
+document.write("<br>");
+document.write(x<=y);true
+document.write("<br>");
+document.write(x==y);false
+document.write("<br>");
+document.write(x===y);false
+document.write("<br>");
+document.write(x!=y);true
+document.write("<br>");
+document.write(x!==y);true
+*/
+/*
+ // logival OPerator
+var x =40;
+var y =30;
+var z =44;
+var a =false;
+document.write(x<y)&&(x>y); false;
+document.write("<br>");
+document.write(x>y)||(x<y);true;
+document.write("<br>");
+document.write(!a);
+document.write("<br>");
+ */
+
+//conditional Operator
+//if(condition){
+    
+//}
+
+
+
+var x=39;
+var y=35;
+var z=60;
+if(x<y){
+    document.write('bangladesh');
+}else if(z<y){
+    document.write('love u dubai');
+}else if((y>x)||(x>y)){
+         document.write('dara-dubai');
+}else{
+    document.write('Not found');
+}
+    
+
